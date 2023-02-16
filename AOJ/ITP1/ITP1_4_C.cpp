61afab34f7699aca03bacd7cf5468813367ae335
nakano0518 <m15sc018.prog@gmail.com>
2023-02-16 19:59:46 +0900

    AOJ ITP1 1~4

--- AOJ/ITP1/ITP1_4_C.cpp
+++ AOJ/ITP1/ITP1_4_C.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-    
+    int a, b;
+    char op;
+    while(cin >> a >> op >> b) {
+        if(op == '?') {
+            break;
+        }
+        if(op == '+') {
+            cout << a + b << endl;
+        }
+        else if(op == '-'){
+            cout << a - b << endl;
+        }else if(op == '*') {
+            cout << a * b << endl;
+        }else if(op == '/') {
+            cout << a / b << endl;
+        }
+    }
 }
--- AOJ/ITP1/ITP1_4_D.cpp
+++ AOJ/ITP1/ITP1_4_D.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto& ai: a) {
+        cin >> ai;
+    }
+    int minimum = *min_element(a.begin(), a.end());
+    int maximum = *max_element(a.begin(), a.end());
+    long long sum = accumulate(a.begin(), a.end(), 0LL);
+    // 第三引数は0ではなく0LL。0であればintで認識されオーバーフローでWAだった。
+   
+
+    cout << minimum << " " << maximum << " " << sum << endl;
+
+    // C++の標準ライブラリ使いこなせてたほうがいい
+    // https://zenn.dev/reputeless/books/standard-cpp-for-competitive-programming
+
 }
